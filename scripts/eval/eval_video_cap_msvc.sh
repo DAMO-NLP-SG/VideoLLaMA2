5614b989a543886c9a8b7891194e876a3145f661
sennnnn <cyanlaser@stu.pku.edu.cn>
2024-06-17 23:01:40 +0800

    Refactor msvc.

---
+++ scripts/eval/eval_video_cap_msvc.sh
@@ -0,0 +1,67 @@
+set -x
+
+EVAL_DATA_DIR=eval
+OUTPUT_DIR=eval_output
+CKPT_NAME=VideoLLaMA2-7B
+CKPT=DAMO-NLP-SG/${CKPT_NAME}
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+# divide data via the number of GPUs per task
+GPUS_PER_TASK=1
+CHUNKS=$((${#GPULIST[@]}/$GPUS_PER_TASK))
+
+output_file=${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/merge.json
+
+# judge if the number of json lines is 0
+if [ ! -f "$output_file" ] || [ $(cat "$output_file" | wc -l) -eq 0 ]; then
+    rm -f ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/*.json
+fi
+
+if [ ! -f "$output_file" ]; then
+    for IDX in $(seq 0 $((CHUNKS-1))); do
+        # select the GPUs for the task
+        gpu_devices=$(IFS=,; echo "${GPULIST[*]:$(($IDX*$GPUS_PER_TASK)):$GPUS_PER_TASK}")
+        TRANSFORMERS_OFFLINE=1 CUDA_VISIBLE_DEVICES=${gpu_devices} python3 videollama2/eval/inference_video_cap_msvc.py \
+          --model-path ${CKPT} \
+          --video-folder ${EVAL_DATA_DIR}/MSVC \
+          --question-file ${EVAL_DATA_DIR}/MSVC/msvc.json \
+          --output-file ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/${CHUNKS}_${IDX}.json \
+          --num-chunks $CHUNKS \
+          --chunk-idx $IDX &
+    done
+
+    wait
+
+    # Clear out the output file if it exists.
+    > "$output_file"
+
+    #Loop through the indices and concatenate each file.
+    for IDX in $(seq 0 $((CHUNKS-1))); do
+        cat ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/${CHUNKS}_${IDX}.json >> "$output_file"
+    done
+fi
+
+
+AZURE_API_KEY=""
+AZURE_API_ENDPOINT=""
+AZURE_API_DEPLOYNAME=""
+
+python3 videollama2/new_eval/eval_video_cap_msvc_correctness.py \
+    --pred-path $output_file \
+    --output-dir ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/correctness_gpt \
+    --output-json ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/correctness_results.json \
+    --api-key $AZURE_API_KEY \
+    --api-endpoint $AZURE_API_ENDPOINT \
+    --api-deployname $AZURE_API_DEPLOYNAME \
+    --num-tasks 4 \
+
+python3 videollama2/new_eval/eval_video_cap_msvc_detailedness.py \
+    --pred-path $output_file \
+    --output-dir ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/detailedness_gpt \
+    --output-json ${OUTPUT_DIR}/MSVC/answers/${CKPT_NAME}/detailedness_results.json \
+    --api-key $AZURE_API_KEY \
+    --api-endpoint $AZURE_API_ENDPOINT \
+    --api-deployname $AZURE_API_DEPLOYNAME \
+    --num-tasks 4 \
