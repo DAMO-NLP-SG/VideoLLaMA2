4d1a77c2462a7f5c9dfe49268a7a0d23d5579481
sennnnn <cyanlaser@stu.pku.edu.cn>
2024-07-06 17:24:26 +0800

    Fix egoschema evaluation bugs.

--- videollama2/eval/inference_video_mcqa_egoschema.py
+++ videollama2/eval/inference_video_mcqa_egoschema.py
@@ -86,7 +86,7 @@ def egoschema_dump(ans_file, line, outputs):
 
         pred_answer = re.findall('[\(\ ]*[A-E][\)\ ]*', output)
         try:
-            assert len(pred_answer) >= 1, 'The video \"{}\" output \"{}\" is not in the expected format'.format(line['video_path'], instruct + '\n' + output)
+            assert len(pred_answer) >= 1, 'The video \"{}\" output \"{}\" is not in the expected format'.format(line['q_uid'], instruct + '\n' + output)
             pred_answer = pred_answer[0].strip()
             if pred_answer.startswith('('):
                 pred_answer = pred_answer.strip('()')
